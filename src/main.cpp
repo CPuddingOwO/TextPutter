6a969d83097bec160d6f3ceafd3c0503fef7ee9c
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2024-12-20 16:16:27 +0800

    entrypoint main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <TextPutter/Application.hpp>
+#include <spdlog/spdlog.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    spdlog::set_level(spdlog::level::info);
+    spdlog::set_pattern("[%H:%M:%S.%e]%^[%L] %v%$");
+
+    spdlog::info("████████╗███████╗██╗  ██╗████████╗██████╗ ██╗   ██╗████████╗████████╗███████╗██████╗");
+    spdlog::info("╚══██╔══╝██╔════╝╚██╗██╔╝╚══██╔══╝██╔══██╗██║   ██║╚══██╔══╝╚══██╔══╝██╔════╝██╔══██╗");
+    spdlog::info("   ██║   █████╗   ╚███╔╝    ██║   ██████╔╝██║   ██║   ██║      ██║   █████╗  ██████╔╝");
+    spdlog::info("   ██║   ██╔══╝   ██╔██╗    ██║   ██╔═══╝ ██║   ██║   ██║      ██║   ██╔══╝  ██╔══██╗");
+    spdlog::info("   ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝      ╚═════╝    ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝");
+
+    tp::Application app;
+    app.run();
     return 0;
 }
