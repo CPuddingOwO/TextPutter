59f7a350437490dca87ed14e61f6d0c7b0b0a9c1
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2024-12-21 14:38:52 +0800

    添加了宏 __WIN32 以隐藏命令行窗口

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <TextPutter/Application.hpp>
 #include <spdlog/spdlog.h>
+#include <Windows.h>
 
+#ifdef __WIN32
+int WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow ) {
+#else
 int main() {
+#endif
+
+    // 重定向标准输出和标准错误
+//    freopen("nul", "w", stdout);
+//    freopen("nul", "w", stderr);
+
     spdlog::set_level(spdlog::level::info);
     spdlog::set_pattern("[%H:%M:%S.%e]%^[%L] %v%$");
 
